6c9313e85d8adbe20a74a4d70100f3979b138ba4
AnabelCarranza <anabelcarranza@gmail.com>
2020-08-05 15:55:48 -0700

    agrego funcion 1

---
+++ index.js
@@ -0,0 +1,53 @@
+const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
+const ventas = [
+    [100000000, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000',
+        'Motherboard ASUS 1500']],
+    [100000001, 1, 1, 2019, 'Ada', 'Centro', ['Monitor GPRS 3000',
+        'Motherboard ASUS 1500']],
+    [100000002, 2, 1, 2019, 'Grace', 'Caballito', ['Monitor ASC 543',
+        'Motherboard MZI', 'HDD Toyiva']],
+    [100000003, 10, 1, 2019, 'Ada', 'Centro', ['Monitor ASC 543',
+        'Motherboard ASUS 1200']],
+    [100000004, 12, 1, 2019, 'Grace', 'Caballito', ['Monitor GPRS 3000',
+        'Motherboard ASUS 1200']],
+    [100000005, 21, 3, 2019, 'Hedy', 'Caballito', ['Monitor ASC 543',
+        'Motherboard ASUS 1200', 'RAM Quinston']]
+]
+
+const precios = [
+    ['Monitor GPRS 3000', 200],
+    ['Motherboard ASUS 1500', 120],
+    ['Monitor ASC 543', 250],
+    ['Motherboard ASUS 1200', 100],
+    ['Motherboard MZI', 30],
+    ['HDD Toyiva', 90],
+    ['HDD Wezter Dishital', 75],
+    ['RAM Quinston', 110],
+    ['RAM Quinston Fury', 230]
+];
+const sucursales = ['Centro', 'Caballito']
+
+
+// 1. precioMaquina(componentes): recibe un array de componentes y devuelve el
+// precio de la máquina que se puede armar con esos componentes, que es la suma
+// de los precios de cada componente incluido
+
+const buscarPrecioPorComponente = (componente) => {
+    const indexPrecio = precios.findIndex(precioComponente => precioComponente[0] === componente);
+    if (indexPrecio === -1) throw new Error('El precio solicitado no existe')
+    return precios[indexPrecio][1];
+};
+
+const precioMaquina = (componentes) => {
+    return componentes.reduce((acumulador, componente) => acumulador + buscarPrecioPorComponente(componente), 0);
+ 
+//     let total = 0;
+//     componentes.forEach((componente, index) => {
+//         const precio = buscarPrecioPorComponente(componente);
+//         total = total + precio;
+//     }); 
+//     return total;
+};
+
+
+console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]));
